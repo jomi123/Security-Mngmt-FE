40b30dc2eb3e4e3effacb9de398e8eea82e055c3
Jomi Susan Mathew <73014817+jomi123@users.noreply.github.com>
2024-11-10 23:32:47 +0530

    refact: env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@ export const environment = {
       clientId: '13cd173a-2f30-45a1-8ee8-9b0028cf7f37',
       authority:
         'https://login.microsoftonline.com/5b751804-232f-410d-bb2f-714e3bb466eb',
-      redirectUri: process.env['NG_APP_REDIRECT_URL'],
-      postLogoutRedirectUri: process.env['NG_APP_REDIRECT_URL'],
+      redirectUri: 'https://security-mngmt-fe.vercel.app',
+      postLogoutRedirectUri: 'https://security-mngmt-fe.vercel.app',
     },
   },
   apiConfig: {
@@ -14,6 +14,6 @@ export const environment = {
     uri: 'https://graph.microsoft.com/v1.0/me',
   },
   serverConfig: {
-    baseUrl: process.env['NG_APP_BACKEND_URL'],
+    baseUrl: 'http://172.16.4.89:9000',
   },
 };
