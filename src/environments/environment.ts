ab435ae920ed9d984534eeed4723d5d55dcc84af
Jomi Susan Mathew <73014817+jomi123@users.noreply.github.com>
2024-11-11 09:49:30 +0530

    fix: error in sso

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,7 @@ export const environment = {
       clientId: '13cd173a-2f30-45a1-8ee8-9b0028cf7f37',
       authority:
         'https://login.microsoftonline.com/5b751804-232f-410d-bb2f-714e3bb466eb',
-      redirectUri:
-        'https://security-mngmt-fe.vercel.app/13cd173a-2f30-45a1-8ee8-9b0028cf7f37',
+      redirectUri: 'https://security-mngmt-fe.vercel.app',
       postLogoutRedirectUri: 'https://security-mngmt-fe.vercel.app',
     },
   },
